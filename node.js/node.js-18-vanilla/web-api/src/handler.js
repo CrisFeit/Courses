3d34f5aa2f9120a0878b252e710809a74f2f8beb
crisfeit <crisrfab@gmail.com>
2022-11-14 23:55:26 -0300

    chore: node.js 18 vanilla api

--- node.js/node.js-18-vanilla/web-api/src/handler.js
+++ node.js/node.js-18-vanilla/web-api/src/handler.js
@@ -1,7 +1,11 @@
+import { parse } from 'node:url'
+
 function handler(request, response) {
-    const {
-        url, method
-    } = request
+    const { url, method } = request
+
+    const { pathname } = parse(url, true)
+
+    console.log('pathname', pathname)
     console.log({ url, method })
     response.end('Hello World')
 }
